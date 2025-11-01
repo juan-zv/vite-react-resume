3a93bc2722e51ddceb66092a4ba9c64e70783d88
juan-zv <juanzuritavasquez@gmail.com>
2025-11-01 14:23:32 -0600

    updated base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vite-react-resume/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
